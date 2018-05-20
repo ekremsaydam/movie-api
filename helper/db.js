98513fc081f4b88453199a03fa4e4f0b09dc1514
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2018-05-21 00:58:08 +0300

    ilk commit

---
+++ helper/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  mongoose.connect('mongodb://news:abcd1234@ds229790.mlab.com:29790/movie-api');
+  mongoose.connection.on('open', () => {
+    console.log('MongoDB : Baglandı');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.log('MongoDB: ERROR', err);
+  });
+};
